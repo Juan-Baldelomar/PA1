a47232a18d75b0aed79354353e90b6b51a2f119c
Juan Luis <jlbc95@gmail.com>
2020-10-18 22:01:19 -0500

    main with code to prove functionality

--- main.c
+++ main.c
@@ -3,6 +3,57 @@
 #include <stdlib.h>
 #include "arbolRN.h"
 
+int getRandom(int base, int limit) {
+    return base + rand() % (limit - base + 1);
+}
+
 int main(){
+    RBTree *rbTree = createTree();
+    Nodo *nodo;
+    int n = 100;
+    int nodesToDelete[100];
+
+    for (int i = 0; i<n; i++){
+        int x = getRandom(0, 1000);
+        nodesToDelete[i] = x;
+        put(rbTree, x, x);
+    }
+
+    // put some keys that we know for sure that exist
+    put(rbTree, 2500, 2500);
+    put(rbTree, 3000, 3000);
+
+    printf(" ********************************************** TREE IN ORDER **********************************************\n");
+    traverse(rbTree);
+
+    delete(rbTree, 2500);
+    nodo = search(rbTree, 2500);
+    if (nodo = rbTree->NIL){
+        printf("Nodo 2500 no existe. (Funcionando Correctamente)\n");
+    }
+
+    nodo = search(rbTree, 3000);
+    if (nodo != rbTree->NIL){
+        printf("Nodo 3000 existe con valor: %d\n", nodo->val);
+    }
+
+    delete(rbTree, 3000);
+    nodo = search(rbTree, 3000);
+    if (nodo = rbTree->NIL){
+        printf("Nodo 3000 no existe. (Funcionando Correctamente)\n");
+    }
+
+
+    printf("Tree size: %d", size(rbTree));
+
+    printf(" *************************************** We are going to delete every node ***************************************\n");
+    for (int i = 0; i<n; i++){
+        delete(rbTree, nodesToDelete[i]);
+    }
+
+    printf("Is Tree Empty:%d\n", isEmpty(rbTree));
+    traverse(rbTree);
+
+    freeTree(&rbTree);
     return 0;
 }
